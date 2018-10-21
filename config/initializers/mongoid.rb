0a830aee2f02cf05a1c4f42f191ae994d7cd3bbe
Santiago Peña <spenas@MacBook-Pro-de-Santiago.local>
2018-10-21 18:05:46 -0500

    changed mongoid

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -3,7 +3,7 @@ module Mongoid
     module Document
       def as_json(options={})
         attrs = super(options)
-        attrs["id"] = attrs["_id"].to_s
+        attrs["soid"] = attrs["_id"].to_s
         attrs
       end
     end
